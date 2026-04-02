1513757391f99f27ca6bcc50b387aaf3f342ccf5
naoki20260401-aiproject <naokisep9281@gmail.com>
2026-04-02 16:22:24 +0900

    Allow Vercel Blob image domains in Next.js Image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.vercel-storage.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.public.blob.vercel-storage.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
